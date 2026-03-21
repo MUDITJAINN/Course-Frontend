f5e07126fa385ab28ee5e51f2a6b8ccc420a08fe
Mudit jain <muditj214@gmail.com>
2026-03-21 15:46:26 +0530

    changes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://course-backend-k9sm.onrender.com/api/v1";  https://course-backend-xwu2.onrender.com
-export const BACKEND_URL = "https://course-backend-xwu2.onrender.com";
+// export const BACKEND_URL = "https://course-backend-k9sm.onrender.com/api/v1";
+export const BACKEND_URL = "https://course-backend-xwu2.onrender.com/api/v1";
 // export const BACKEND_URL = "http://localhost:4001/api/v1";
