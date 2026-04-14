a178a6fe22c70def339e3233ad5bf19e53cbaf92
Mudit jain <muditj214@gmail.com>
2026-04-14 18:14:49 +0530

    Code Fixes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
-// export const BACKEND_URL = "https://course-backend-xwu2.onrender.com/api/v1";
-export const BACKEND_URL = "http://localhost:4001/api/v1";
+export const BACKEND_URL = "https://course-backend-xwu2.onrender.com/api/v1";
+// export const BACKEND_URL = "http://localhost:4001/api/v1";
 
 export const EMAILJS_SERVICE_ID = "service_tqxz5z7";
 export const EMAILJS_TEMPLATE_ID = "template_rt1f0ha";
